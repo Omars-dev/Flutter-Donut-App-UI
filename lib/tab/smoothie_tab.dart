a1087903f59f700a0063a2845ff9af178a5a76b0
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-03 23:30:10 +0600

    update

--- lib/tab/smoothie_tab.dart
+++ lib/tab/smoothie_tab.dart
@@ -4,7 +4,7 @@ import 'package:flutter_donut_app_ui/utils/smoothie_tile.dart';
 class SmoothieTab extends StatelessWidget {
   SmoothieTab({Key? key}) : super(key: key);
 
-  List pizzaOnSale = [
+  List smoothieOnSale = [
     ["🍓 Banana", "50", Colors.pink, "assets/images/strawberry_smoothie.png"],
     ["Blueberry 🌰", "75", Colors.blue, "assets/images/blueberry_smoothie.png"],
     ["Cherry Almond", "34", Colors.red, "assets/images/cherry-smoothie.png"],
@@ -16,15 +16,15 @@ class SmoothieTab extends StatelessWidget {
     return GridView.builder(
         scrollDirection: Axis.vertical,
         padding: const EdgeInsets.all(12),
-        itemCount: pizzaOnSale.length,
+        itemCount: smoothieOnSale.length,
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: 2, childAspectRatio: 1 / 1.5),
         itemBuilder: (context, index) {
           return SmoothieTile(
-            smoothieFlavor: pizzaOnSale[index][0],
-            smoothiePrice: pizzaOnSale[index][1],
-            smoothieColor: pizzaOnSale[index][2],
-            imageName: pizzaOnSale[index][3],
+            smoothieFlavor: smoothieOnSale[index][0],
+            smoothiePrice: smoothieOnSale[index][1],
+            smoothieColor: smoothieOnSale[index][2],
+            imageName: smoothieOnSale[index][3],
           );
         });
   }
