d2dc68410103104ad070de0d30e169135d7c1319
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-04 23:04:18 +0600

    update

---
+++ lib/views/screen_ui/cart.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){},
+          icon: Icon(Icons.arrow_back, color: Colors.black,)
+        ),
+      ),
+    );
+  }
+}
