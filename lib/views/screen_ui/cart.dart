6feff8c6b2144a02c7a8440d52a1c6b42b3f9f73
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-05 00:24:35 +0600

    update

--- lib/views/screen_ui/cart.dart
+++ lib/views/screen_ui/cart.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_donut_app_ui/theme_style/app_constants.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -16,10 +17,78 @@ class CartPage extends StatelessWidget {
           icon: Icon(Icons.chevron_left, color: Color(0xFF424242),)
         ),
         actions: [
-          IconButton(onPressed: (){},
-              padding: EdgeInsets.only(right: 20),
-              icon: Icon(Icons.share, color: Color(0xFF424242),)
+          Container(
+            padding: EdgeInsets.all(3),
+            margin: EdgeInsets.only(right: 24,top: 8),
+            decoration: BoxDecoration(
+              color: Colors.pink[100],
+              borderRadius: BorderRadius.circular(100)
+            ),
+            child: IconButton(onPressed: (){},
+                icon: Icon(Icons.share, color: Color(0xFF424242),
+                  size: 18,
+                )
+            ),
           )
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 25),
+          Container(
+            margin: EdgeInsets.all(20),
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: themeColor,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 85,
+                  width: 85,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12)
+                  ),
+                ),
+                SizedBox(width: 20,),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 20,
+                      width: 280,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Container(
+                      height: 15,
+                      width: 180,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                    ),
+                    SizedBox(height: 18),
+                    Container(
+                      height: 12,
+                      width: 80,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(12)
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
