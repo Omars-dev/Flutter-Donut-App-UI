755d02f45bc0e99f04610739681406ce8d51d1d3
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-06 10:16:00 +0600

    update

--- lib/views/screen_ui/cart.dart
+++ lib/views/screen_ui/cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_donut_app_ui/theme_style/app_constants.dart';
+import 'package:flutter_donut_app_ui/widgets/appbar/product_details_appbar.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -7,89 +8,94 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: IconButton(
-          onPressed: (){
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.chevron_left, color: Color(0xFF424242),)
-        ),
-        actions: [
-          Container(
-            padding: EdgeInsets.all(3),
-            margin: EdgeInsets.only(right: 24,top: 8),
-            decoration: BoxDecoration(
-              color: Colors.pink[100],
-              borderRadius: BorderRadius.circular(100)
-            ),
-            child: IconButton(onPressed: (){},
-                icon: Icon(Icons.share, color: Color(0xFF424242),
-                  size: 18,
-                )
-            ),
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          SizedBox(height: 25),
-          Container(
-            margin: EdgeInsets.all(20),
-            padding: EdgeInsets.all(12),
-            decoration: BoxDecoration(
-              color: themeColor,
-              borderRadius: BorderRadius.circular(20)
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 85,
-                  width: 85,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(12)
-                  ),
+      backgroundColor: Colors.white,
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.transparent,
+      //   leading: IconButton(
+      //     onPressed: (){
+      //       Navigator.pop(context);
+      //     },
+      //     icon: Icon(Icons.chevron_left,size: 34, color: Color(0xFF424242),)
+      //   ),
+      //   actions: [
+      //     Container(
+      //       margin: EdgeInsets.only(right: 24,top: 10),
+      //       decoration: BoxDecoration(
+      //         color: Colors.pink[50],
+      //         borderRadius: BorderRadius.circular(100)
+      //       ),
+      //       child: IconButton(onPressed: (){},
+      //           icon: Icon(Icons.delete_rounded, color: Color(0xFF424242),
+      //             size: 30,
+      //           )
+      //       ),
+      //     )
+      //   ],
+      // ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const DetailsAppbar(),
+              const SizedBox(height: 25),
+              Container(
+                margin: const EdgeInsets.all(20),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: themeColor,
+                  borderRadius: BorderRadius.circular(20)
                 ),
-                SizedBox(width: 20,),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                child: Row(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      height: 20,
-                      width: 280,
+                      height: 85,
+                      width: 85,
                       decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(12)
                       ),
                     ),
-                    SizedBox(height: 10),
-                    Container(
-                      height: 15,
-                      width: 180,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(12)
-                      ),
-                    ),
-                    SizedBox(height: 18),
-                    Container(
-                      height: 12,
-                      width: 80,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12)
-                      ),
+                    const SizedBox(width: 20,),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 20,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        Container(
+                          height: 15,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12)
+                          ),
+                        ),
+                        const SizedBox(height: 18),
+                        Container(
+                          height: 12,
+                          width: 80,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12)
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/appbar/product_details_appbar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class DetailsAppbar extends StatelessWidget {
+  const DetailsAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 56,
+      child: Row(
+        children: [
+          GestureDetector(
+            onTap: (){},
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              child: Icon(Icons.chevron_left,color: Colors.black38,size: 32,),
+            ),
+          ),
+          const Spacer(),
+          GestureDetector(
+            onTap: (){},
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.only(right: 20,top: 12),
+              decoration: BoxDecoration(
+                color: Colors.pink[50],
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: const Icon(Icons.delete_rounded,size: 32,color: Colors.black38,),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
