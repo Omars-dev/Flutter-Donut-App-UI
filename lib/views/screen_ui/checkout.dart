51aa003891016c80a7d53c4d41c70f04d5b843bd
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-13 17:50:34 +0600

    update

--- lib/views/screen_ui/checkout.dart
+++ lib/views/screen_ui/checkout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_donut_app_ui/theme_style/app_constants.dart';
 
 class CheckoutPage extends StatelessWidget {
   const CheckoutPage({Key? key}) : super(key: key);
@@ -11,16 +12,60 @@ class CheckoutPage extends StatelessWidget {
         elevation: 0,
         leading: IconButton(
             onPressed: (){},
-            icon: Icon(Icons.chevron_left,color: Colors.black38,size: 32,),
+            icon: const Icon(Icons.chevron_left,color: Colors.black54,size: 32,),
         ),
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(24),
-        child: Text('Checkout',
-            style: TextStyle(
-                fontWeight: FontWeight.w700,
-                color: Colors.black,
-                fontSize: 40)),
+      body: ListView(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(24),
+            child: Text('Checkout',
+                style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    color: Colors.black,
+                    fontSize: 30)),
+          ),
+          Container(
+            padding: const EdgeInsets.all(16),
+            margin: const EdgeInsets.symmetric(horizontal: 24),
+            decoration: BoxDecoration(
+              color: themeColor,
+                borderRadius: BorderRadius.circular(20)
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    const Text('Delivery Address'),
+                    const Spacer(),
+                    Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: const Icon(Icons.edit_outlined,size: 22)
+                    ),
+                  ],
+                ),
+                //Address
+                Row(
+                  children: [
+                    const Spacer(),
+                    Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50)
+                        ),
+                        child: const Icon(Icons.edit_outlined,size: 22)
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
