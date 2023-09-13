b02a080153ecf9d4e7b2cb5ee291de8597ae82d7
Omar Faruk <omarfaruks2018@gmail.com>
2023-09-13 17:11:01 +0600

    update

--- lib/views/screen_ui/checkout.dart
+++ lib/views/screen_ui/checkout.dart
@@ -5,13 +5,23 @@ class CheckoutPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-          child: Text('Checkout',
-              style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  color: Colors.black,
-                  fontSize: 40))),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: (){},
+            icon: Icon(Icons.chevron_left,color: Colors.black38,size: 32,),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(24),
+        child: Text('Checkout',
+            style: TextStyle(
+                fontWeight: FontWeight.w700,
+                color: Colors.black,
+                fontSize: 40)),
+      ),
     );
   }
 }
